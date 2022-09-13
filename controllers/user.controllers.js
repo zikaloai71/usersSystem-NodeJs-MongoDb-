23bc0d33bb7804a51c2c0efb8fa62aa7e2e04854
zikaloai71 <Ze.Lo.Moukhtar@el-eng.menofia.edu.eg>
2022-09-13 17:46:09 +0200

    add and show users features added

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,7 +1,37 @@
 const myConnection = require("./myConnection");
 const { ObjectId } = require("mongodb");
 class User {
+  static index = (req, res) => {
+    myConnection((db) => {
+      db.collection("users")
+        .find()
+        .toArray((e, result) => {
+          if (e) return console.log(e.message);
+          res.render("home", {
+            pageTitle: "home page",
+            result,
+            noUsers: result.length === 0 ? true : false,
+          });
+        });
+    });
+  };
 
+  static add = (req, res) => {
+    res.render("add", {
+      pageTitle: "add page",
+    });
+  };
+
+  static getAddLogic = (req, res) => {
+    let user = { ...req.query };
+
+    myConnection((db) => {
+      db.collection("users")
+        .insertOne(user)
+        .then(res.redirect("/"))
+        .catch((e) => console.log(e.message));
+    });
+  };
 }
 
 module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const User = require("../controllers/user.controllers");
+router.get("/", User.index);
 
+router.get("/add", User.add);
+
+router.get("/getAddLogic", User.getAddLogic);
 
 module.exports = router;
